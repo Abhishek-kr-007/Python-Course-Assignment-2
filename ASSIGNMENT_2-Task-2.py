cdb4aa465fc9db2fd47e427abb6647e74ad6aadf
Abhishek Kumar <abhishekkumar.ak2580@gmail.com>
2025-03-31 11:24:00 +0530

    Add files via upload

---
+++ ASSIGNMENT_2-Task-2.py
@@ -0,0 +1,8 @@
+def sum_numbers():
+    total = 0
+    for num in range(1, 51):
+        total += num
+    print(f"The sum of numbers from 1 to 50 is: {total}")
+
+if __name__ == "__main__":
+    sum_numbers()
